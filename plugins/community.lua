7068e73888f89ca5d8f5b3548a7910f4ae010bac
d1en25 <sdemianenko25@gmail.com>
2023-09-25 13:35:30 +0300

    astrocommunity catppuccin bar

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,12 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity/bars-and-lines/lualine-nvim" },
+  { import = "astrocommunity/bars-and-lines/heirline-vscode-winbar" },
+  { import = "astrocommunity/pack/typescript" },
+  { import = "astrocommunity/pack/json" },
+  { import = "astrocommunity/pack/bash" },
+  { import = "astrocommunity/pack/html-css" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
